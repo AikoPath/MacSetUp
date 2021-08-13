720bb03564dda059ada0fe0fbb1d95f2eaf009cf
Aiko Klostermann <aklostermann@apple.com>
2021-08-13 13:00:38 +0800

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,27 @@
+echo “Install brew”
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo
+echo “Installing iTerm 2”
+brew install --cask iterm2
+
+echo “Installing Stats”
+brew install --cask stats
+
+echo
+echo “Installing Brave”
+brew install --cask brave-browser
+
+echo
+echo “Installing 1Password”
+brew install --cask 1password
+
+echo
+echo “Installing Maccy clipboard manager”
+brew install --cask maccy
+
+echo
+echo “TODO:”
+echo “Iterm2: preferences: General -> Closing -> uncheck ‘Confirm Quit iTerm …’ ”
+echo “Configure Maccy…”
+
