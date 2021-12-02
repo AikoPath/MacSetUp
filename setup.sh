1975d04bf8b4097a2e7917d93e8938f07479274f
Aiko <aiko@aiko.dev>
2021-12-02 10:39:16 +0800

    Update set up comments

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 echo “Install brew”
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/aiko/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 echo
 echo “Installing iTerm 2”
@@ -21,12 +23,17 @@ echo “Installing Maccy clipboard manager”
 brew install --cask maccy
 
 echo
+echo "Installing NordVPN"
+brew install --cask nordvpn
+
+echo
 echo "Installing Oh My Zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-
 echo
 echo “TODO:”
-echo “Iterm2: preferences: General -> Closing -> uncheck ‘Confirm Quit iTerm …’ ”
+echo “iterm: preferences: General -> Closing - uncheck Confirm Quit iTerm … ”
+echo "iTerm: preferences: Appearance: - Dimming affects only text, not backgound."
+echo "iTerm: preferences: Profiles: General: - Working directory - Reuse previous session's directory"
 echo “Configure Maccy…”
 
